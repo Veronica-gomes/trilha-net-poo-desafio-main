1accca47ae2a46d3cfff82f913b51dfd8047ae7d
Veronica Gomes <88216333+Veronica-gomes@users.noreply.github.com>
2022-11-14 14:17:51 -0300

    Add files via upload

---
+++ trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,11 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia n1 = new Nokia("+55 (11)910096886", "Nokiaphone", "n212d0", 128);
+n1.Ligar();
+n1.InstalarAplicativo("WhatsApp");
+
+Iphone i1 = new Iphone("+55 (11) 94002-8922", "Iphone X", "5451d", 100);
+i1.InstalarAplicativo("Websupply");
+i1.ReceberLigacao();
